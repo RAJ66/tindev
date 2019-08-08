c5a321b3e41283906137a354a0cdc9fea40ab8e1
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-08 14:16:38 +0100

    Login page

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -7,11 +7,15 @@
  */
 
 import React from 'react';
-import {Text} from 'react-native';
+
+import Login from './pages/Login'
+
 
 export default function App() {
   return (
-    <Text>Hello world</Text>
+    <Login/>
   );
 }
+
+
 
