126d5bfb1229a99d4ae71e790a601e44449f6aef
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-08 15:47:22 +0100

    Login complete

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -8,12 +8,12 @@
 
 import React from 'react';
 
-import Login from './pages/Login'
+import Routes from './routes';
 
 
 export default function App() {
   return (
-    <Login/>
+    <Routes/>
   );
 }
 
--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
-import { KeyboardAvoidingView,Platform, Text,StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import api from '../service/api';
 
 import logo from '../assets/logo.png';
 
 
-export default function Login() {
 
-  return <KeyboardAvoidingView 
-  behavior="padding"
-  enabled={Platform.OS==='ios'}
-  style={style.container}>
+
+export default function Login({ navigation }) {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    })
+  }, [])
+
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user });
+
+    const { _id } = response.data;
+    await AsyncStorage.setItem('user', _id);
+    navigation.navigate('Main', { _id });
+  }
+
+  return <KeyboardAvoidingView
+    behavior="padding"
+    enabled={Platform.OS === 'ios'}
+    style={style.container}>
     <Image source={logo} />
 
-    <TextInput 
-    autoCapitalize="none"
-    autoCorrect={false}
-    placeholder="Digite seu usuario do github"
+    <TextInput
+      autoCapitalize="none"
+      autoCorrect={false}
+      placeholder="Digite seu usuario do github"
       placeholderTextColor="#999"
       style={style.input}
+      value={user}
+      onChangeText={setUser}
     />
 
-    <TouchableOpacity style={style.button}>
-    <Text style={style.buttonText}>Enviar</Text>
+    <TouchableOpacity onPress={handleLogin} style={style.button}>
+      <Text style={style.buttonText}>Enviar</Text>
     </TouchableOpacity>
   </KeyboardAvoidingView>;
 
@@ -46,19 +69,19 @@ const style = StyleSheet.create({
     marginTop: 20,
     paddingHorizontal: 15,
   },
-  button:{
+  button: {
     height: 46,
     alignSelf: 'stretch',
     backgroundColor: '#df4723',
     borderRadius: 4,
-    marginTop:10,
-    justifyContent:'center',
-    alignItems:'center',
-    
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+
   },
-  buttonText:{
-    color:'#fff',
-    fontWeight:'bold',
-    fontSize:16,
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 })
