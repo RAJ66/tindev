4c1be7e3deab0e8a56bfa9e925690b213c7a0b2a
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-02-09 21:34:25 +0000

    beginning Switch

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import api from "../../services/api";
 
 import logo from "../../assets/logo.svg";
 
 import { Button, LoginContainer, Form, Input } from "./styles.js";
+import Switch from "react-switch";
+import { ThemeContext } from "styled-components";
 
 export default function Login({ history }) {
   const [username, setUsername] = useState("");
+  const { colors } = useContext(ThemeContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -28,6 +31,19 @@ export default function Login({ history }) {
           onChange={e => setUsername(e.target.value)}
         />
         <Button type="submit">Enviar</Button>
+        <Switch
+          onChange={() => {}}
+          checked={true}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          height={10}
+          width={40}
+          handleDiameter={20}
+          offColor="#000"
+          onColor="#000"
+          onHandleColor={colors.primary}
+          offHandleColor={colors.primary}
+        />
       </Form>
     </LoginContainer>
   );
